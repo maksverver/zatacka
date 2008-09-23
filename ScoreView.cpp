68e6504dce731b46215699996daa0a702beedb0a
Maks Verver <maksverver@geocities.com>
2008-09-23 22:25:21 +0200

    Fixed GUI bugs and a server bug.

--- ScoreView.cpp
+++ ScoreView.cpp
@@ -15,30 +15,37 @@ void ScoreView::update( std::vector<std::string> names,
                         std::vector<int> scores,
                         std::vector<Fl_Color> colors )
 {
-
     clear();
+    nameLabels.clear();
+    scoreLabels.clear();
     begin();
 
-    int x = this->x() + 5, y = this->y() + 5, w = this->w() - 10, h = 40;
+    /* hack: allocate strings */
     for (size_t n = 0; n < names.size(); ++n)
     {
         assert(n < scores.size());
         assert(n < colors.size());
+        nameLabels.push_back("  " + names[n]);
+        static char scoreStr[64];
+        sprintf(scoreStr, "%d  ", scores[n]);
+        scoreLabels.push_back(scoreStr);
+    }
 
+    int x = this->x() + 5, y = this->y() + 5, w = this->w() - 10, h = 40;
+    for (size_t n = 0; n < names.size(); ++n)
+    {
         Fl_Box *box = new Fl_Box(x, y, w, h);
         box->box(FL_RSHADOW_BOX);
         box->color(colors[n]);
-        box->label(names[n].c_str());
+        box->label(nameLabels[n].c_str());
         box->labelfont(FL_HELVETICA);
         box->labelsize(20);
         box->labelcolor(FL_WHITE);
         box->align(FL_ALIGN_INSIDE | FL_ALIGN_LEFT);
 
-        char scoreStr[64];
-        sprintf(scoreStr, "%d", scores[n]);
-        box = new Fl_Box(x + w - 30, y + 5, 30, h);
+        box = new Fl_Box(x + w - 30, y + 2, 30, h);
         box->color(colors[n]);
-        box->label(scoreStr);
+        box->label(scoreLabels[n].c_str());
         box->labelfont(FL_HELVETICA);
         box->labelsize(24);
         box->labelcolor(FL_WHITE);
@@ -46,6 +53,7 @@ void ScoreView::update( std::vector<std::string> names,
 
         y += 50;
     }
+
     end();
     redraw();
 }
