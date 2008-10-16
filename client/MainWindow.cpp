66d8f2007d4a3fb9bc7276e51f7afd63ffde1158
Maks Verver <maks@hell.student.utwente.nl>
2008-10-16 23:18:51 +0200

    Updated Makefiles for building with FLTK 1.3. Added frame rate counter to client.

--- client/MainWindow.cpp
+++ client/MainWindow.cpp
@@ -8,12 +8,17 @@ MainWindow::MainWindow( int width, int height,
     label("Zatacka!");
     color(fl_gray_ramp(FL_NUM_GRAY/4));
     gv = new MainGameView(2, 2, height - 4, height - 4, antialiasing);
-    sv = new ScoreView(height, 0, width - height, height - 20);
+    sv = new ScoreView(height, 0, width - height, height - 40);
     gid_box = new Fl_Box(height, height - 20, width - height, 20);
     gid_box->labelfont(FL_HELVETICA);
     gid_box->labelsize(12);
     gid_box->labelcolor(fl_gray_ramp(2*FL_NUM_GRAY/3));
     gid_box->align(FL_ALIGN_INSIDE);
+    fps_box = new Fl_Box(height, height - 40, width - height, 20);
+    fps_box->labelfont(FL_HELVETICA);
+    fps_box->labelsize(12);
+    fps_box->labelcolor(fl_gray_ramp(2*FL_NUM_GRAY/3));
+    fps_box->align(FL_ALIGN_INSIDE);
     end();
     if (fullscreen) this->fullscreen();
 }
@@ -35,6 +40,13 @@ void MainWindow::setGameId(unsigned gid)
 {
     sprintf(gid_box_label, "%08X", gid);
     gid_box->label(gid_box_label);
+}
+
+void MainWindow::setFPS(double fps)
+{
+    if (fps > 999) fps = 999;
+    sprintf(fps_box_label, "%.3f FPS", fps);
+    fps_box->label(fps_box_label);
 }
 
 void MainWindow::resetGameView(int players)
