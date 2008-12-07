a247394edc98ead605ad03155724bd8ac4b345d4
unknown <Maks Verver@.(none)>
2008-12-07 17:56:37 +0100

    Removed incorrect MSVC implementation of time_now()

--- common/Time.c
+++ common/Time.c
@@ -1,26 +1,21 @@
 #include "Time.h"
 #include <stdlib.h>
 
-#ifdef _MSC_VER
-#include <time.h>
-#else
+#ifndef _MSC_VER
 #include <sys/time.h>
 #endif
 
-
 #ifdef _MSC_VER
 
-static clock_t c_start = 0;
-
-/* Resets the time counter */
 void time_reset()
 {
-    c_start = clock();
+    /* unimplemented */
 }
 
 double time_now()
 {
-    return (double)(clock() - c_start)/(double)CLOCKS_PER_SEC;
+    /* unimplemented */
+    return 0.0;
 }
 
 #else
